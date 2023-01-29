346388de736827af8f1a791274f65414167a29d5
Oleg Kishisnkii <oleg.kishinskiy@gmail.com>
2023-01-29 14:29:40 +0300

    disable question

--- page.php
+++ page.php
@@ -222,13 +222,13 @@ get_header();
                     </div>
                 </div>
             </div>
-            <div class="question">
+            <!-- <div class="question">
                 <div class="question__text">
                     Есть вопросы? Напишите нам!
                 </div>
                 <div id="reply" class="minibutton">Написать</div>
                 <div class="question__close">&times</div>
-            </div>
+            </div> -->
 
             <div class="reply">
                 <div class="reply__body">
